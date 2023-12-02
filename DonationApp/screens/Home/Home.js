8b24c25756b75adf1009d5894b780af7bbb7c27f
Claytonseraphin <clayton0687@gmail.com>
2023-12-02 09:38:58 -0500

    Created Search Component

--- DonationApp/screens/Home/Home.js
+++ DonationApp/screens/Home/Home.js
@@ -6,17 +6,16 @@ import Header from '../../components/Header/Header';
 import Button from '../../components/Button/Button';
 import Tab from '../../components/Tab/Tab';
 import Badge from '../../components/Badge/Badge';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import Search from '../../components/Search/Search';
 
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-      <Header title={'Emmanuel Michel'} type={1} />
-      <Tab title={'Highlits'} />
-      <Tab title={'Highlits'} isInactive={true} />
-      <Badge title={'Environment'} />
-      <FontAwesomeIcon icon={faSearch} />
+      <Search
+        onSearch={value => {
+          console.log(value);
+        }}
+      />
     </SafeAreaView>
   );
 };
